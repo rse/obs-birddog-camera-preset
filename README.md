d66158fb7596871487617804a8754b3d840e38cb
Ralf S. Engelschall <rse@engelschall.com>
2022-01-07 13:08:20 +0100

    place files under version control

--- README.md
+++ README.md
@@ -1,1 +1,159 @@
-# obs-birddog-preset
+
+OBS-Birddog-Camera-Preset
+=========================
+
+**Recalling a Birddog Camera Preset from within OBS Studio**
+
+About
+-----
+
+This is a small HTML5 Single-Page-Application (SPA) for interactively
+controlling the position and/or size of one or more related *Crop/Pad*
+source filters in [OBS Studio](https://obsproject.com) through a remote
+[OBS WebSocket](https://github.com/obsproject/obs-websocket) connection.
+The transition from the old to the new crop position and/or size is
+performed over a configured time duration and with a cubic in/out
+easing, in order to somewhat simulate the behaviour of a PTZ camera.
+
+This SPA can be running inside a separate Browser, inside a *Custom
+Browser Dock* of [OBS Studio](https://obsproject.com), or even inside a
+[OBS Source Dock](https://github.com/exeldro/obs-source-dock) inside [OBS
+Studio](https://obsproject.com).
+
+Sneak Preview
+-------------
+
+### OBS Crop Control in a Browser
+
+![Screenshot 1](screenshot-1.png)
+
+### OBS Crop Control in Custom Browser Dock of OBS Studio
+
+![Screenshot 2](screenshot-2.png)
+
+### OBS Crop Control in OBS Source Dock of OBS Studio
+
+![Screenshot 3](screenshot-3.png)
+
+Use Case
+--------
+
+The particular, original use-case for this application is the following:
+
+- **High-Resolution Camera & Green-Screen**:
+  You are using a high-resolution (4K, 3840x2160px) camera in front of
+  a physical green-screen and you replace the green-screen with the
+  [OBS Studio](https://obsproject.com) *Chroma-Key* source filter. In
+  OBS Studio, behind your camera, you are using an arbitrary background
+  image or video media (visible through the alpha channel created by the
+  *Chroma-Key* filter).
+
+- **Non-PTZ Camera Functionality**:
+  Your camera is a non-PTZ (Pan-Tilt-Zoom) camera or you at least cannot
+  use the available PTZ functionality of your camera because of the
+  green-screen (different angles and zoom-levels would require the
+  background to adjust simultanously).
+
+- **Simulated Dynamics & Lower-Resolution Cameras**:
+  You want to still somewhat simulate multiple camera angles or zooms
+  by just cropping various Full-HD (1920x1080px) areas out of your 4K
+  (3840x2160px) camera video with the help of the OBS Studio *Crop/Pad*
+  filter. You then treat those areas as some sort of "virtual cameras".
+  In order to dynamically and interactively change the *Crop/Pad*
+  position and size parameters you use this control application.
+
+Setup
+-----
+
+The particular, original setup is the following:
+
+- You are producing your live-event with
+  [OBS Studio](https://obsproject.com) as your free video streaming software.
+
+- You have the [OBS WebSocket](https://github.com/obsproject/obs-websocket),
+  [OBS Source Dock](https://github.com/exeldro/obs-source-dock),
+  and [StreamFX](https://github.com/Xaymar/obs-StreamFX) extension plugins
+  installed and activated in [OBS Studio](https://obsproject.com).
+
+- You have [OBS Studio](https://obsproject.com) configured for Full-HD
+  (1920x1080px) video output (see *Settings* &rarr; *Video* &rarr; *Base (Canvas) Resolution*).
+
+- You have a scene collection in [OBS Studio](https://obsproject.com) configured,
+  which contains at least the following additional scenes for your camera (here named `CAM2` as an example):
+
+  - scene `CAM2-Full`:
+    (rationale: scene for "full/total" camera view)
+      - source `CAM2-Full-CC` of type *Browser Source*:
+          - loading the OBS Crop Control SPA according to the URL below<br/>
+            (rationale: running the SPA)
+      - source `CAM2-Full-FG` of type *Video Capture Device*:
+          - attached to your physical 4K camera device<br/>
+            (rationale: single source for physical camera)
+          - filter *Chrome Key* applied
+            (rationale: single filter for chroma-key)
+          - transform of *Stretch to Screen* applied<br/>
+            (rationale: provide "full/total" camera view in 1080p of `CAM2-Full`)
+      - source `CAM2-Full-BG` of type *Image*:
+          - attached to your 4K background image<br/>
+            (rationale: single filter for chroma-key)
+          - transform of *Stretch to Screen* applied<br/>
+            (rationale: provide "full/total" camera view in 1080p of `CAM2-Full`)
+  - scene `CAM2-Zoom`:
+    (rationale: scene for "zoomed" camera view)
+      - source `CAM2-Zoom-FG` of type *Source Mirror*:
+          - attached to source `CAM2-Full-FG`
+            (rationale: single source for physical camera, and chroma-key already applied once and in 4K)
+          - filter *Crop/Pad* applied<br/>
+            (rationale: the zoom to be applied and controlled)
+          - filter *Scaling/Aspect Ratio* applied (for 1920x1080px)
+            (rationale: ensure result is still Full-HD, even on arbitrary crop areas)
+      - source `CAM2-Zoom-BG` of type *Source Mirror*:
+          - attached to source `CAM2-Full-BG`<br/>
+            (rationale: single source for background)
+          - filter *Crop/Pad* applied<br/>
+            (rationale: the zoom to be applied and controlled)
+          - filter *Scaling/Aspect Ratio* applied (for 1920x1080px)<br/>
+            (rationale: ensure result is still Full-HD, even on arbitrary crop areas)
+
+- You use this SPA in a separate Browser, or directly from within OBS Studio
+  with the help of the *Custom Browser Dock* functionality,
+  or even better, inside a [OBS Source Dock](https://github.com/exeldro/obs-source-dock).
+  The URL (show-casing all parameters) for the SPA is like the following
+  (descriptions at the end of each line):<br/>
+
+  `file://[...]/obs-crop-control.html` (path to SPA)<br/>
+  `?websocket=localhost:4444` (endpoint of OBS WebSocket)<br/>
+  `&password=XXX` (authentication for endpoint of OBS WebSocket)<br/>
+  `&title=CAM2` (title of the virtual camera)<br/>
+  `&transparent=true` (make background transparent)<br/>
+  `&canvas=3840x2160` (size of original camera)<br/>
+  `&preview=CAM2-Full:10` (how to retrieve the original camera view and its FPS)<br/>
+  `&sources=CAM2-Zoom-FG,CAM2-Zoom-BG` (the sources of the *Crop/Pad* filters)<br/>
+  `&define=0:0+0/3860x2160,` (pre-defined total 4K area)<br/>
+  `1:0+540/1920x1080,` (pre-defined Full-HD area middle/left)<br/>
+  `2:960+540/1920x1080,` (pre-defined Full-HD area middle/center)<br/>
+  `3:1920+540/1920x1080` (pre-defined Full-HD area middle/right)
+
+  Notice: the `preview` functionality makes sense only for the case
+  this SPA is running inside a separate Browser or within a *Custom
+  Browser Dock* functionality. When running inside [OBS Source
+  Dock](https://github.com/exeldro/obs-source-dock) there is no need
+  to retrieve the preview into the SPA &mdash; instead just place the
+  preview into the same scene behind the SPA!
+
+Usage
+-----
+
+- Select a crop area with a mouse left click to pick it up.
+- Cancel the selection of a crop area with a mouse right click.
+- Change position of crop area with mouse movement.
+- Change size of crop area with mouse wheel.
+- Alternatively just press the keys of the pre-defined crop areas
+  to directly change selections according to pre-definition.
+
+License
+-------
+
+Copyright &copy; 2022 [Dr. Ralf S. Engelschall](http://engelschall.com/)<br/>
+Distributed under [GPL 3.0 license](https://spdx.org/licenses/GPL-3.0-only.html)
+
---
+++ birddog-camera-preset.lua
@@ -0,0 +1,214 @@
+--[[
+**
+**  birddog-camera-preset.lua -- OBS Studio Source Filter for Recalling a Birddog Camera Preset
+**  Copyright (c) 2022 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**  Distributed under MIT license <https://spdx.org/licenses/MIT.html>
+**
+--]]
+
+--  global OBS API
+local obs = obslua
+
+--  global Lua APIs
+local bit      = require("bit")
+local ljsocket = require("shared-ljsocket")
+
+--  send HTTP request
+local function httpRequest (host, port, path, type, body)
+    --  create HTTP request
+    local req = ""
+    if type == nil and body == nil then
+        req = "GET " .. path .. " HTTP/1.1\r\n"
+    else
+        req = "POST " .. path .. " HTTP/1.1\r\n"
+    end
+    req = req ..
+        "Host: " .. host .. "\r\n" ..
+        "User-Agent: OBS-Studio\r\n" ..
+        "Accept: */*\r\n" ..
+        "Connection: keep-alive\r\n"
+    if not (type == nil and body == nil) then
+        req = req ..
+            "Content-Type: " .. type .."\r\n" ..
+            "Content-Length: " .. string.len(body) .."\r\n" ..
+            "\r\n" ..
+            body
+    end
+
+    --  connect to the server
+    obs.script_log(obs.LOG_INFO, string.format("httpRequest: connect: \"%s:%d\"", host, port))
+    local socket = ljsocket.create("inet", "stream", "tcp")
+    socket:set_blocking(false)
+    socket:connect(host, port)
+    while true do
+        if socket:is_connected() then
+            --  send request
+            obs.script_log(obs.LOG_INFO, string.format("httpRequest: send: \"%s\"", req))
+            local ok, err = socket:send(req)
+            if err == "timeout" then
+                obs.script_log(obs.LOG_INFO, string.format("httpRequest: send: error: %s -- aborting", err))
+                return nil
+            end
+
+            --  receive response
+            local res = ""
+            local total_length = 0
+            while true do
+                local chunk, err = socket:receive()
+                if chunk then
+                    res = res .. chunk
+                    if not total_length then
+                        total_length = tonumber(res:match("Content%-Length: (%d+)"))
+                    end
+                    if #res >= total_length then
+                        obs.script_log(obs.LOG_INFO, string.format("httpRequest: receive: \"%s\"", res))
+                        return res
+                    end
+                elseif err ~= "timeout" then
+                    obs.script_log(obs.LOG_INFO, string.format("httpRequest: receive: error: %s -- aborting", err))
+                    return nil
+                end
+            end
+        else
+            local ok, err = socket:poll_connect()
+            if err ~= "timeout" then
+                obs.script_log(obs.LOG_INFO, string.format("httpRequest: poll: error: %s -- aborting", err))
+                return nil
+            end
+        end
+    end
+end
+
+--  recall a pre-defined PTZ preset on a Birddog camera
+local function recall (address, preset)
+     obs.script_log(obs.LOG_INFO,
+         string.format("recall PTZ preset #%d on Birddog camera %s", preset, address))
+     httpRequest(address, 8080, "/recall",
+         "application/json", "{ \"Preset\": \"Preset-" .. preset .. "\" }")
+end
+
+--  create obs_source_info structure
+local info = {}
+info.id           = "birddog_camera_preset"
+info.type         = obs.OBS_SOURCE_TYPE_FILTER
+info.output_flags = bit.bor(obs.OBS_SOURCE_VIDEO)
+
+--  hook: provide name of filter
+info.get_name = function ()
+    return "Birddog Camera Preset"
+end
+
+--  hook: provide default settings (initialization before create)
+info.get_defaults = function (settings)
+    --  provide default values
+    obs.obs_data_set_default_bool(settings, "program", true)
+    obs.obs_data_set_default_string(settings, "address", "192.168.0.1")
+    obs.obs_data_set_default_int(settings, "preset", 1)
+end
+
+--  hook: create filter context
+info.create = function (_settings, source)
+    --  create new filter context object
+    local filter = {}
+    filter.source = source
+    filter.parent = nil
+    filter.width  = 0
+    filter.height = 0
+    filter.name = obs.obs_source_get_name(source)
+    filter.cfg = {
+        program = true,
+        address = "192.168.0.1",
+        preset  = 1
+    }
+    obs.script_log(obs.LOG_INFO, string.format("hook: create: filter name: \"%s\"", filter.name))
+    return filter
+end
+
+--  hook: destroy filter context
+info.destroy = function (filter)
+    --  free resources only (notice: no more logging possible)
+    filter.source = nil
+    filter.name   = nil
+    filter.cfg    = nil
+end
+
+--  hook: after loading settings
+info.load = function (filter, settings)
+    filter.cfg.program = obs.obs_data_get_bool(settings, "program")
+    filter.cfg.address = obs.obs_data_get_string(settings, "address")
+    filter.cfg.preset  = obs.obs_data_get_int(settings, "preset")
+end
+
+--  hook: provide filter properties (for dialog)
+info.get_properties = function (_filter)
+    --  create properties
+    local props = obs.obs_properties_create()
+    obs.obs_properties_add_bool(props, "program", "Activate preset when scene becomes active in Program only")
+    obs.obs_properties_add_text(props, "address", "Birddog Camera IP (X.X.X.X):", obs.OBS_TEXT_DEFAULT)
+    obs.obs_properties_add_int(props, "preset", "Birddog Camera PTZ Preset (1-9):", 1, 9, 1)
+    return props
+end
+
+--  hook: react on filter property update (during dialog)
+info.update = function (filter, settings)
+    filter.cfg.program = obs.obs_data_get_string(settings, "program")
+    filter.cfg.address = obs.obs_data_get_string(settings, "address")
+    filter.cfg.preset  = obs.obs_data_get_int(settings, "preset")
+end
+
+--  hook: activate (program)
+info.activate = function (filter)
+    if filter.cfg.program then
+        recall(filter.cfg.address, filter.cfg.preset)
+    end
+end
+
+--  hook: show (anywhere)
+info.show = function (filter)
+    if not filter.cfg.program then
+        recall(filter.cfg.address, filter.cfg.preset)
+    end
+end
+
+--  hook: render video
+info.video_render = function (filter, _effect)
+    if filter.parent == nil then
+        filter.parent = obs.obs_filter_get_parent(filter.source)
+    end
+    if filter.parent ~= nil then
+        filter.width  = obs.obs_source_get_base_width(filter.parent)
+        filter.height = obs.obs_source_get_base_height(filter.parent)
+    end
+    obs.obs_source_skip_video_filter(filter.source)
+end
+
+--  hook: provide size
+info.get_width = function (filter)
+    return filter.width
+end
+info.get_height = function (filter)
+    return filter.height
+end
+
+--  register the filter
+obs.obs_register_source(info)
+
+--  script hook: description displayed on script window
+function script_description ()
+    return [[
+        <h2>Birddog Camera Preset</h2>
+
+        Copyright &copy; 2022 <a style="color: #ffffff; text-decoration: none;"
+        href="http://engelschall.com">Dr. Ralf S. Engelschall</a><br/>
+        Distributed under <a style="color: #ffffff; text-decoration: none;"
+        href="https://spdx.org/licenses/MIT.html">MIT license</a>
+
+        <p>
+        <b>Define a Birddog Camera Preset filter for sources. This is intended
+        to allow OBS Studio to force a Birddog camera to recall a pre-defined
+        Pan/Tilt/Zooom (PTZ) preset in case the source becomes visible (aka shown)
+        in any display or visible (aka active) in the Program.</b>
+        </p>
+    ]]
+end
+
