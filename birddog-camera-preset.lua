3a625bdd02d19d30309329400acd9c2fe30669fe
Ralf S. Engelschall <rse@engelschall.com>
2022-01-08 11:55:02 +0100

    allow PREVIEW and PROGRAM to be controlled independently

--- birddog-camera-preset.lua
+++ birddog-camera-preset.lua
@@ -101,9 +101,9 @@ end
 --  hook: provide default settings (initialization before create)
 info.get_defaults = function (settings)
     --  provide default values
-    obs.obs_data_set_default_bool(settings, "program", true)
     obs.obs_data_set_default_string(settings, "address", "192.168.0.1")
-    obs.obs_data_set_default_int(settings, "preset", 1)
+    obs.obs_data_set_default_string(settings, "preset1", "none")
+    obs.obs_data_set_default_string(settings, "preset2", "none")
 end
 
 --  hook: create filter context
@@ -116,9 +116,9 @@ info.create = function (_settings, source)
     filter.height = 0
     filter.name = obs.obs_source_get_name(source)
     filter.cfg = {
-        program = true,
         address = "192.168.0.1",
-        preset  = 1
+        preset1 = "none",
+        preset2 = "none"
     }
     obs.script_log(obs.LOG_INFO, string.format("hook: create: filter name: \"%s\"", filter.name))
     return filter
@@ -134,39 +134,79 @@ end
 
 --  hook: after loading settings
 info.load = function (filter, settings)
-    filter.cfg.program = obs.obs_data_get_bool(settings, "program")
+    --  take current parameters
     filter.cfg.address = obs.obs_data_get_string(settings, "address")
-    filter.cfg.preset  = obs.obs_data_get_int(settings, "preset")
+    filter.cfg.preset1 = obs.obs_data_get_string(settings, "preset1")
+    filter.cfg.preset2 = obs.obs_data_get_string(settings, "preset2")
+
+    --  hook: activate (preview)
+    obs.obs_frontend_add_event_callback(function (ev)
+        if ev == obs.OBS_FRONTEND_EVENT_PREVIEW_SCENE_CHANGED then
+            if filter.parent ~= nil then
+                local sceneSource      = obs.obs_frontend_get_current_preview_scene()
+                local sceneSourceName  = obs.obs_source_get_name(sceneSource)
+                local filterSourceName = obs.obs_source_get_name(filter.parent)
+                local scene            = obs.obs_scene_from_source(sceneSource)
+                local sceneItem        = obs.obs_scene_find_source_recursive(scene, filterSourceName)
+                obs.obs_source_release(sceneSource)
+                if sceneItem then
+                    if filter.cfg.preset1 ~= "none" then
+                        obs.script_log(obs.LOG_INFO, string.format(
+                            "hook: scene \"%s\" with filter source \"%s\" is in PREVIEW now -- reacting",
+                            sceneSourceName, filterSourceName))
+                        recall(filter.cfg.control, filter.cfg.preset1)
+                    end
+                end
+            end
+        end
+    end)
 end
 
 --  hook: provide filter properties (for dialog)
 info.get_properties = function (_filter)
     --  create properties
     local props = obs.obs_properties_create()
-    obs.obs_properties_add_bool(props, "program", "Activate preset when scene becomes active in Program only")
     obs.obs_properties_add_text(props, "address", "Birddog Camera IP (X.X.X.X):", obs.OBS_TEXT_DEFAULT)
-    obs.obs_properties_add_int(props, "preset", "Birddog Camera PTZ Preset (1-9):", 1, 9, 1)
+    local function addList (prop)
+        obs.obs_property_list_add_string(prop, "none", "none")
+        obs.obs_property_list_add_string(prop, "1", "1")
+        obs.obs_property_list_add_string(prop, "2", "2")
+        obs.obs_property_list_add_string(prop, "3", "3")
+        obs.obs_property_list_add_string(prop, "4", "4")
+        obs.obs_property_list_add_string(prop, "5", "5")
+        obs.obs_property_list_add_string(prop, "6", "6")
+        obs.obs_property_list_add_string(prop, "7", "7")
+        obs.obs_property_list_add_string(prop, "8", "8")
+        obs.obs_property_list_add_string(prop, "9", "9")
+    end
+    local preset1 = obs.obs_properties_add_list(props, "preset1", "Birddog Camera PTZ Preset on PREVIEW:",
+        obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
+    local preset2 = obs.obs_properties_add_list(props, "preset2", "Birddog Camera PTZ Preset on PROGRAM:",
+        obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
+    addList(preset1)
+    addList(preset2)
     return props
 end
 
 --  hook: react on filter property update (during dialog)
 info.update = function (filter, settings)
-    filter.cfg.program = obs.obs_data_get_string(settings, "program")
     filter.cfg.address = obs.obs_data_get_string(settings, "address")
-    filter.cfg.preset  = obs.obs_data_get_int(settings, "preset")
+    filter.cfg.preset1 = obs.obs_data_get_string(settings, "preset1")
+    filter.cfg.preset2 = obs.obs_data_get_string(settings, "preset2")
 end
 
 --  hook: activate (program)
 info.activate = function (filter)
-    if filter.cfg.program then
-        recall(filter.cfg.address, filter.cfg.preset)
-    end
-end
-
---  hook: show (anywhere)
-info.show = function (filter)
-    if not filter.cfg.program then
-        recall(filter.cfg.address, filter.cfg.preset)
+    if filter.cfg.preset2 ~= "none" then
+        if filter.parent ~= nil then
+            local sceneSource      = obs.obs_frontend_get_current_scene()
+            local sceneSourceName  = obs.obs_source_get_name(sceneSource)
+            local filterSourceName = obs.obs_source_get_name(filter.parent)
+            obs.script_log(obs.LOG_INFO, string.format(
+                "hook: scene \"%s\" with filter source \"%s\" is in PROGRAM now -- reacting",
+                sceneSourceName, filterSourceName))
+            recall(filter.cfg.address, filter.cfg.preset2)
+        end
     end
 end
 
@@ -204,10 +244,10 @@ function script_description ()
         href="https://spdx.org/licenses/GPL-3.0-only.html">GPL 3.0 license</a>
 
         <p>
-        <b>Define a Birddog Camera Preset filter for sources. This is intended
+        Define a <b>Birddog Camera Preset</b> filter for sources. This is intended
         to allow OBS Studio to force a Birddog camera to recall a pre-defined
-        Pan/Tilt/Zooom (PTZ) preset in case the source becomes visible (aka shown)
-        in any display or visible (aka active) in the Program.</b>
+        Pan/Tilt/Zooom (PTZ) preset in case the source becomes visible
+        in the PREVIEW (for enabled studio mode only) and/or PROGRAM.
         </p>
     ]]
 end
